10bc89864a952468fe51c9d24f7a51d0526c83eb
evalopezm <evalopezm>
2020-04-07 10:23:26 +0200

    painted elements except img

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 const Pokemon = (props) => {
-  console.log(props.pokemon.name);
+  console.log(props.pokemon.url);
+
+  const pokemonTypes = props.pokemon.types.map((type, index) => {
+    return <li key={index}>{type}</li>;
+  });
 
   return (
     <li key={props.pokemon.id}>
+      <img url="{props.pokemon.url}" alt="" />
       <h2>{props.pokemon.name}</h2>
+      <h3>{pokemonTypes}</h3>
     </li>
   );
 };

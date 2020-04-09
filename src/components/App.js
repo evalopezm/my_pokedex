040ef531a36aa46d37d4af8bb711027fbd1e506d
evalopezm <evalopezm>
2020-04-09 21:50:02 +0200

    :sparkles: add footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "../stylesheets/App.css";
 import pokemonArray from "../pokemonArray";
 import PokeList from "../components/PokeList";
+import Footer from "./footer";
 
 const App = () => {
   const [pokemons] = useState(pokemonArray);
@@ -9,6 +10,7 @@ const App = () => {
     <>
       <h2 className="title">Mi lista de pokemon</h2>
       <PokeList pokemonArray={pokemons} />
+      <Footer />
     </>
   );
 };
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../stylesheets/footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr></hr>
+      <p className="copyright">© Eva López Merino</p>
+    </div>
+  );
+};
+
+export default Footer;
